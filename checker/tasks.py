84bd2c7df88e60761d7f19d902da8290d493267f
maksim <6234513@gmail.com>
2023-08-23 16:25:08 +0300

    update check_file_errors func

--- checker/tasks.py
+++ checker/tasks.py
@@ -10,10 +10,13 @@ logger = get_task_logger('__name__')
 @shared_task
 def check_file_errors(file_id):
     file = UploadedFile.objects.get(id=file_id)
-    (pylint_stdout, pylint_stderr) = lint.py_run(f"{file.file.path}", return_std=True)
+    (pylint_stdout, pylint_stderr) = lint.py_run(f"{file.file.path}"
+                                                 f" --disable=import-error,"
+                                                 f"missing-module-docstring,"
+                                                 f"missing-function-docstring,"
+                                                 f"missing-class-docstring "
+                                                 f"--max-line-length=120", return_std=True)
     output = pylint_stdout.getvalue()
     logger.info(output)
     print(output)
     return output
-
-
