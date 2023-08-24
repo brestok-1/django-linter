44bf2b7d5694fba1c00edef2c18ddc9b4e81bcec
maksim <6234513@gmail.com>
2023-08-24 21:57:14 +0300

    create index.html

--- checker/views.py
+++ checker/views.py
@@ -7,18 +7,18 @@ from checker.tasks import check_file_errors
 
 
 # Create your views here.
-# class IndexView(TemplateView):
-#     template_name = 'checker/index.html'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         # context['data'] = check_file_errors.delay(1)
-#         return context
-#
-#
-# class FilesView(LoginRequiredMixin, ListView):
-#     template_name = 'checker/files.html'
-#     def get_queryset(self):
-#         queryset = UploadedFile.objects.filter(user=self.request.user)
-#         return queryset
+class IndexView(TemplateView):
+    template_name = 'checker/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['data'] = check_file_errors.delay(1)
+        return context
+
+
+class FilesView(LoginRequiredMixin, ListView):
+    template_name = 'checker/files.html'
+    def get_queryset(self):
+        queryset = UploadedFile.objects.filter(user=self.request.user)
+        return queryset
 
